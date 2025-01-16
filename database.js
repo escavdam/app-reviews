e15e98d821f798b63e767dc79796720d41d38718
datadiego <juandiegomariscal@gmail.com>
2025-01-16 09:43:35 +0100

    fix: script de inicializacion bbdd

--- database.js
+++ database.js
@@ -1,22 +1,16 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const Database = require('better-sqlite3');
-const fs = require('fs');
-let db_aux = null;
-if(!fs.existsSync('database.db')){
-    db_aux = new Database('database.db');//cambia por database.db para guardar en un archivo
-    db_aux.exec("CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT)");
+const db = new Database('database.db');
 
-    db_aux.exec(`
-        CREATE TABLE reviews(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        time DATETIME DEFAULT CURRENT_TIMESTAMP, 
-        rating TEXT NOT NULL, 
-        message TEXT, 
-        username TEXT, 
-        FOREIGN KEY(username) REFERENCES users(username)
-    `)
+db.exec("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)");
 
-} else {
-    db_aux = new Database('database.db');
-}
+db.exec(`
+    CREATE TABLE IF NOT EXISTS reviews(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    rating TEXT NOT NULL, 
+    message TEXT, 
+    username TEXT, 
+    FOREIGN KEY(username) REFERENCES users(username)
+)`);
