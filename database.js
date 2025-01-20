5d62122df495fa2cd87424f47635630457ace990
lucasst626 <lucasst626@gmail.com>
2025-01-20 09:35:28 +0100

    fix is admin

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const saltRounds = 10;
 const Database = require('better-sqlite3');
 const db = new Database('database.db');
 
-db.exec("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT, isAdmin BOOLEAN)");
+db.exec("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)");
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS reviews(
@@ -18,9 +18,10 @@ db.exec(`
 const adminPasswordHash = '$2b$10$w0.NRKi/vPsV8py49sD34.G4xY6np7aQjB4BHmBkZGDrL7zSAVY3m';
 const adminUsername = 'admin';
 
+// Insertar el usuario administrador directamente
 try {
-    const insert = db.prepare("INSERT INTO users (username, password, isAdmin) VALUES (?, ?, ?)");
-    insert.run(adminUsername, adminPasswordHash, true);
+    const insert = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    insert.run(adminUsername, adminPasswordHash);
     console.log('Admin user created');
 } catch (err) {
     console.error('Error initializing admin user:', err);
